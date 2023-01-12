3b802d3ae0ebd9d0bb709ce3a67768960f2ee291
Seal <a05852002@hotmail.com>
2023-01-12 17:24:08 +0800

    no message

---
+++ src/main/java/springTeam5/SpringBootTeam5Application.java
@@ -0,0 +1,13 @@
+package springTeam5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeam5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTeam5Application.class, args);
+	}
+
+}
